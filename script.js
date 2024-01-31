babbfab0633ba352f9e761972b273f08ce448689
Afrakhan22 <afrakhanam22@gmail.com>
2024-01-31 22:15:08 +0530

    add:page3

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var a = document.querySelector("#elem1")
+var image= a.getAttribute("data-image")
+console.log(image)
